bd6f70c654c4e1793b2255600a3124832daa8721
Vincent Feng <fengwenshenging@gmail.com>
2021-10-18 15:48:43 +0800

    add discover:search ui

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # flutter_agora
 flutter播客应用。
 ## 项目预览截图
-| ![](preview/home.jpg) | ![](preview/tab_title.jpg) |
-| --------------------- | -------------------------- |
+| ![](preview/home.jpg) | ![](preview/tab_title.jpg) | ![](preview/discover_search.jpeg) |
+| --------------------- | -------------------------- | --------------------------------- |
--- lib/app/modules/home/discover/discover_view.dart
+++ lib/app/modules/home/discover/discover_view.dart
@@ -1,19 +1,40 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import 'discover_controller.dart';
 
 class DiscoverView extends GetView<DiscoverController> {
+  void _search(String keywords) => log('$keywords');
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'DiscoverView is working',
-          style: TextStyle(fontSize: 20),
+  Widget build(_) => Container(
+        alignment: Alignment.topCenter,
+        color: Colors.white,
+        child: Container(
+          alignment: Alignment.center,
+          height: 40.0,
+          margin: EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.grey[300]!,
+              width: 1.5,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          ),
+          child: TextField(
+            onSubmitted: _search,
+            cursorColor: Colors.black,
+            decoration: InputDecoration(
+              hintText: 'Search a podcast or feed',
+              contentPadding: EdgeInsets.symmetric(
+                horizontal: 16.0,
+              ),
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
         ),
-      ),
-    );
-  }
+      );
 }
